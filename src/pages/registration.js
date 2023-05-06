8816a401206c0c3dce21a10913eb24fe5f736125
alina-koptelova <alina0901200432ya.ru>
2023-05-06 20:10:57 +0500

    updade

--- src/pages/registration.js
+++ src/pages/registration.js
@@ -1,17 +1,19 @@
 import Head from 'next/head'
 import styles from './registration.module.scss'
 
-export{};
+export function Button(props) {
+  return <button className={`${styles.button}
+  `}>{props.caption}</button>
+}
 
 export default function Home() {
     return (
-      <>
       <div className={styles.container}>
         <Head>
             <meta charset="UTF-8"/>
-            <link rel="preconnect" href="https://fonts.googleapis.com"/>
-            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
-            <link href="https://fonts.googleapis.com/css2?family=Montserrat+Alternates:wght@500&display=swap" rel="stylesheet"></link>
+            <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin></link>
+            <link href="https://fonts.googleapis.com/css2?family=Sedgwick+Ave+Display&display=swap" rel="stylesheet"></link>
             <link rel="preconnect" href="https://fonts.googleapis.com"/>
             <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
             <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@400;500&display=swap" rel="stylesheet"></link>
@@ -19,10 +21,32 @@ export default function Home() {
             <meta name="viewport" content="width=device-width, initial-scale=1"/>
         </Head>
         <main>
-            <div className={styles.registrationPage}>Привет</div>
+          <div className={styles.registrationPage}>
+            <form action='http://localhost:3000/login' className={styles.button__login}>
+              <Button caption='Вход'></Button>
+            </form>
+            <div className={styles.registrationPage__title}>
+              Underground
+            </div>
+            <form action="process_registration.php" method="post">
+              <div className={styles.form}>
+                <div className={styles.form__title}>Регистрация</div>
+                <label for="fullname">Никнейм</label>
+                <input type="text" id="fullname" name="fullname" required></input>
+                <label for="email">Почта</label>
+                <input type="email" id="email" name="email" required></input>
+                <label for="password">Пароль</label>
+                <input type="password" id="password" name="password" required></input>
+                <label for="password">Повторите пароль</label>
+                <input type="password" id="password" name="password" required></input>
+                <div className={styles.button__send}>
+                  <Button type="submit" caption='Отправить'></Button>
+                </div>
+              </div>
+            </form>
+          </div>  
         </main>
       </div>
-      </>
     )
   }
   
